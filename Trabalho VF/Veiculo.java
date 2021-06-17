3469585be820a8b9657d0b6ce6f88d6f1ba7ee70
yasmin livino <78516836+Maly-Livino@users.noreply.github.com>
2021-06-17 01:40:37 +0000

    Modificações

--- Trabalho VF/Veiculo.java
+++ Trabalho VF/Veiculo.java
@@ -1,54 +1,65 @@
 import java.time.LocalDate;
 
-public class Veiculo {
+public abstract class Veiculo {
 
-    private final TipoDeVeiculo tipoDeVeiculo;
-    private final Motor motor;
-    private Motorista motoristaAtual;
+      private String cor;
+      private String Marca;
+      private Array Tanque; 
+      private enum direction{
+        Direita
+        Esquerda
+      } 
 
-    public Veiculo(TipoDeVeiculo tipoDeVeiculo, Motor motor) {
-        if (tipoDeVeiculo == null)
-            throw new NullPointerException("Precisa passar um tipo de veiculo!!");
-        this.tipoDeVeiculo = tipoDeVeiculo;
-        this.motor = motor;
+      private enum FarolState{
+        Desligado
+        FarolBaixo
+        FarolAlto
+      } 
+
+     private enum InternalSetStatefarol( enum FarolState ){
+
+     } 
+
+     private direction InternalSetDirection( enum direction ){
+      return direction; 
+      // tem que me devolver direta ou esquerda
+     }   
+
+     private Boolean InternalLigarCarro(){
+         
+     } 
+
+     private Boolean InternalDesligarCarro(){
+
+     }
+
+     private Boolean InternalAbastecer(){
+
+     } 
+
+    public Veiculo(String cor, String Marca) {
+        
     }
 
-    public TipoDeVeiculo getTipoDeVeiculo() {
-        return tipoDeVeiculo;
-    }
+   public Boolean Ligar(){     
+     return InternalLigarCarro(); 
+   }
 
-    public Motor getMotor() {
-        return motor;
-    }
+   public Boolean Desligar(){     
+      return  InternalDesligarCarro();
 
-    public Motorista getMotoristaAtual() {
-        return motoristaAtual;
-    }
+   public Boolean Abastecer(){
+      return InternalAbastecer();
+   }
 
-    public void dirigir(Motorista motorista) {
-        if (motorista == null) {
-            throw new NullPointerException("Um carro não se dirige sozinho... motorista é nulo!");
-        }
+   public enum Seta(){
+    return InternalSetDirection();
 
-        motoristaAtual = motorista;
+   }
 
-        if (tipoDeVeiculo == TipoDeVeiculo.BICICLETA) {
-            System.out.println("bicicleta sendo pilotada pelo " + motorista.getNome());
-        } else {
-            LocalDate vencimentoCNH = motorista.getVencimentoCNH();
-            if (vencimentoCNH.isBefore(LocalDate.now())) {
-                throw new InfracaoException(motorista.getNome() + " com CNH vencida!");
-            }
-
-            motor.ligar();
-            System.out.println(tipoDeVeiculo + " sendo dirigido pelo " + motorista.getNome());
-        }
-    }
-
-    public void estacionar() {
-        motor.desligar();
-        motoristaAtual = null;
-        System.out.println(tipoDeVeiculo + " estacionado");
-    }
+   public Veiculo Farol ( enum FarolState ){
+    return InternalSetStatefarol();
+      
+   }
 
 }
---
+++ Trabalho VF/carro.java
@@ -0,0 +1,13 @@
+import Veiculo;
+
+public class Carro extends Veiculo{
+    public Carro(String cor, String Marca, Array Tanque) {
+        super(cor, Marca);
+    }
+}
+
+class Ford89 extends Veiculo(){
+
+
+  
+}
